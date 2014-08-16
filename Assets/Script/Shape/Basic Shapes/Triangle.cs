91027948a8440fcfdce6d6a69d966af907321c97
JZTym <justincfcristi@yahoo.com>
2014-08-16 18:14:44 +0800

    Added triangle

---
+++ Assets/Script/Shape/Basic Shapes/Triangle.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Triangle : Shape {
+
+	#region variables
+	
+	private	const	float	m_speed			= 64.0f;
+	private			Vector3	m_center		= Vector3.zero;
+
+	#endregion // variables
+	
+	#region Monobehavior Functions
+	
+	private void Start ()
+	{
+		m_center = this.transform.position + Vector3.one;
+	}
+	
+	// Update is called once per frame
+	protected override void _Update () 
+	{
+		//apply state
+		if (m_canBeMoved)
+		{
+			this.transform.RotateAround(m_center, Vector3.forward, (m_speed*Time.deltaTime));
+		}
+	}
+	
+	#endregion // Monobehavior Functions
+}
